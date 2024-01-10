2ab3887cc7d349e316f1331c60fdc2841396659b
sundae <94350080+dqyydq@users.noreply.github.com>
2024-01-10 19:48:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,13 +19,87 @@ import os
 os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /workshop/extra-work/OpenLMLab/sentence-transformer')
 download(model_repo='OpenLMLab/InternLM-7b')
 
+# 获取文件路径函数
+def get_files(dir_path):
+    # args：dir_path，目标文件夹路径
+    file_list = []
+    for filepath, dirnames, filenames in os.walk(dir_path):
+        # os.walk 函数将递归遍历指定文件夹
+        for filename in filenames:
+            # 通过后缀名判断文件类型是否满足要求
+            if filename.endswith(".md"):
+                # 如果满足要求，将其绝对路径加入到结果列表
+                file_list.append(os.path.join(filepath, filename))
+            elif filename.endswith(".txt"):
+                file_list.append(os.path.join(filepath, filename))
+    return file_list
+
+# 加载文件函数
+def get_text(dir_path):
+    # args：dir_path，目标文件夹路径
+    # 首先调用上文定义的函数得到目标文件路径列表
+    file_lst = get_files(dir_path)
+    # docs 存放加载之后的纯文本对象
+    docs = []
+    # 遍历所有目标文件
+    for one_file in tqdm(file_lst):
+        file_type = one_file.split('.')[-1]
+        if file_type == 'md':
+            loader = UnstructuredMarkdownLoader(one_file)
+        elif file_type == 'txt':
+            loader = UnstructuredFileLoader(one_file)
+        else:
+            # 如果是不符合条件的文件，直接跳过
+            continue
+        docs.extend(loader.load())
+    return docs
+
+def creat_Database()
+    # 目标文件夹
+    tar_dir = [
+        "/workshop/extra-work/InternLM",
+        "/workshop/extra-work/InternLM-XComposer",
+        "/workshop/extra-work/lagent",
+        "/workshop/extra-work/lmdeploy",
+        "/workshop/extra-work/opencompass",
+        "/workshop/extra-work/xtuner"
+    ]
+    
+    # 加载目标文件
+    docs = []
+    for dir_path in tar_dir:
+        docs.extend(get_text(dir_path))
+    
+    # 对文本进行分块
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500, chunk_overlap=150)
+    split_docs = text_splitter.split_documents(docs[:10])
+    
+    # 加载开源词向量模型
+    embeddings = HuggingFaceEmbeddings(model_name="/workshop/extra-work/OpenLMLab/sentence-transformer")
+    
+    # 构建向量数据库
+    # 定义持久化路径
+    persist_directory = '/workshop/extra-work/data_base/vector_db/chroma'
+    # 加载数据库
+    vectordb = Chroma.from_documents(
+        documents=split_docs,
+        embedding=embeddings,
+        persist_directory=persist_directory  # 允许我们将persist_directory目录保存到磁盘上
+    )
+    # 将加载的向量数据库持久化到磁盘上
+    vectordb.persist()
+
+
 def load_chain():
     # 加载问答链
     # 定义 Embeddings
     embeddings = HuggingFaceEmbeddings(model_name="/workshop/extra-work/OpenLMLab/sentence-transformer")
-
+    test_dir="/workshop/extra-work/data_base/vector_db/chroma"
+    if not os.path.exists(test_dir):
+        creat_Database()
     # 向量数据库持久化路径
-    persist_directory = 'data_base/vector_db/chroma'
+    persist_directory = '/workshop/extra-work/data_base/vector_db/chroma'
 
     # 加载数据库
     vectordb = Chroma(
